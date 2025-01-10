f956d09886851c93040a5e00ea13597654dcc63c
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-09 21:06:02 -0300

    changes: alter name folder

---
+++ dataSstructure/VariAbles.js
@@ -0,0 +1,11 @@
+var num = 1; // {1}
+num = 3; // {2}
+var price = 1.5; // {3}
+var myName = 'Packt'; // {4}
+var trueValue = true; // {5}
+var nullVar = null; // {6}
+
+
+console.log(typeof(price)); // {7}
+
+console.log('price' + ' ' + price); // {8}
