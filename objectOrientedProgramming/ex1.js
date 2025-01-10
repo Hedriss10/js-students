b07b283fa60f7eb8319a5e66115d1acae32b73b1
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-10 13:42:02 -0300

    changes

---
+++ objectOrientedProgramming/ex1.js
@@ -0,0 +1,27 @@
+/*
+Create class with js
+*/
+
+class Pearson{
+    constructor(username= String, lastname= String, age=String, city=String, email=String){
+        this.username = username;
+        this.lastname = lastname;
+        this.age = age;
+        this.city = city;
+        this.email = email 
+    }
+
+    // get age 
+    getAge(){
+        if (this.age < 18){
+            return 'Age not valid';
+        }
+    }
+}
+
+
+const register = new Pearson('Jhonatan', 'Pereira', 17, 'Sao Paulo', 'jhpereira@sp.com');
+const age = register.getAge();
+
+console.log(register);
+console.log(age);
