747f8981c89a48e62108cdf9be0b177bf6b67af7
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-10 12:43:44 -0300

    changes

---
+++ objectOrientedProgramming/Constructor.js
@@ -0,0 +1,21 @@
+
+// class Book(title, author, year) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+// }
+
+
+class Book {
+    constructor(title=String, author=String, year=String) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
+
+var book = new Book('Clean Code', 'Robert', '2011');
+
+Book.prototype.getSummary = function myFucntion() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+ }
