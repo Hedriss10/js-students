8d6ca02e98f00818a97ad9a77e9431f710e54457
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-13 11:43:22 -0300

    changes: add escopes data structure

--- dataStructure/Escopes.js
+++ dataStructure/Escopes.js
@@ -15,3 +15,37 @@ console.log(myVariable); // {5}
 console.log(myOtherVariable); // {6}
 console.log(myFunction()); // {7}
 console.log(myOtherFunction()); // {8}
+
+
+
+
+
+let movie = 'Lord of the rings'; // {1}
+
+function startWansFans(){
+    const movie = 'Start Wars'; // {2}
+    return movie
+}
+
+function marvelFans(){
+    const movie = 'Avengers'; // {3}
+    return movie
+}
+
+function blizzardFans(){    
+    const isFan = true;
+    let pharse = 'Warcraft'; // {4}
+
+    if (isFan) {
+        let pharse = 'Starcraft'; // {5}
+        pharse =  'For the horde!'
+        console.log('Inside if' + pharse);
+    }
+    pharse = 'For the alliance!'
+    console.log('Outside if: ' + pharse);
+}
+
+console.log(movie)
+console.log(startWansFans())
+console.log(marvelFans())
+blizzardFans()
